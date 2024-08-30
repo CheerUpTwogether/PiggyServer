9606947dda64b5c35719256b7e73bf1f67bbcb46
mymain838 <hades517111@gmail.com>
2024-08-30 15:47:46 +0900

    [feat] #5 로그인 테스트를 위한 vercel 배포

---
+++ api/vertify.js
@@ -0,0 +1,22 @@
+const argon2 = require("argon2");
+
+module.exports = async (req, res) => {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method Not Allowed" });
+  }
+
+  try {
+    const { sid, nickname, hash } = req.body;
+    if (!sid || !nickname || !hash) {
+      return res.status(400).json({ error: "옳바르지 않은 값입니다." });
+    }
+    const secretSaltKey = process.env.SALT;
+    const password = `${nickname}${sid}${secretSaltKey}`;
+    const isMatch = await argon2.verify(hash, password);
+
+    res.status(200).json({ data: isMatch });
+  } catch (error) {
+    console.error("Error occurred:", error);
+    res.status(500).json({ error: "서버 요청 오류" });
+  }
+};
