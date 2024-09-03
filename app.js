c1bca0d2e699b337bfe93b065a295ab4a96eed8f
mymain838 <hades517111@gmail.com>
2024-09-04 00:36:02 +0900

    [fix] #18 알림 api 로직 수정

--- app.js
+++ app.js
@@ -24,11 +24,16 @@ const https = require("https");
 const fs = require("fs");
 
 // HTTPS 인증관련
-var privateKey = fs.readFileSync("/etc/letsencrypt/live/www.piggynative.kro.kr/privkey.pem")
-var certificate = fs.readFileSync("/etc/letsencrypt/live/www.piggynative.kro.kr/cert.pem")
-var ca = fs.readFileSync("/etc/letsencrypt/live/www.piggynative.kro.kr/chain.pem")
-const credentials = { key: privateKey, cert: certificate, ca: ca }
-
+var privateKey = fs.readFileSync(
+  "/etc/letsencrypt/live/www.piggynative.kro.kr/privkey.pem"
+);
+var certificate = fs.readFileSync(
+  "/etc/letsencrypt/live/www.piggynative.kro.kr/cert.pem"
+);
+var ca = fs.readFileSync(
+  "/etc/letsencrypt/live/www.piggynative.kro.kr/chain.pem"
+);
+const credentials = { key: privateKey, cert: certificate, ca: ca };
 
 const app = express();
 const port = 3000;
@@ -48,51 +53,57 @@ async function scheduleNotification(messagePayload) {
 app.post("/sendNotification", async (req, res) => {
   const { id, message, time } = req.body;
 
-  // 타임 문자열을 Date 객체로 변환
-  const year = parseInt(time.substring(0, 4));
-  const month = parseInt(time.substring(4, 6)) - 1; // 월은 0부터 시작
-  const day = parseInt(time.substring(6, 8));
-  const hour = parseInt(time.substring(8, 10));
-  const minute = parseInt(time.substring(10, 12));
-  const second = parseInt(time.substring(12, 14));
+  // Supabase에서 디바이스 토큰 조회
+  const { data, error } = await supabase
+    .from("users_nickname")
+    .select("device_token")
+    .eq("id", id);
 
-  const scheduleTime = new Date(year, month, day, hour, minute, second);
+  if (error) {
+    return res.status(500).json({ error: "유저의 토큰을 찾을 수 없습니다." });
+  }
 
-  try {
-    // Supabase에서 디바이스 토큰 조회
-    const { data, error } = await supabase
-      .from("users_nickname")
-      .select("device_token")
-      .eq("id", id);
+  if (data.length === 0) {
+    return res.status(404).json({ error: "디바이스 토큰을 찾을 수 없습니다." });
+  }
 
-    if (error) {
-      return res.status(500).json({ error: "유저의 토큰을 찾을 수 없습니다." });
-    }
+  // 각 토큰으로 FCM 메시지 전송
+  const messagePayload = {
+    notification: {
+      title: message.title,
+      body: message.body,
+    },
+    token: data[0].device_token,
+  };
 
-    if (data.length > 0) {
-      // 각 토큰으로 FCM 메시지 전송
-      const messagePayload = {
-        notification: {
-          title: message.title,
-          body: message.body,
-        },
-        token: data[0].device_token,
-      };
+  if (time) {
+    // 타임 문자열을 Date 객체로 변환
+    const year = parseInt(time.substring(0, 4));
+    const month = parseInt(time.substring(4, 6)) - 1; // 월은 0부터 시작
+    const day = parseInt(time.substring(6, 8));
+    const hour = parseInt(time.substring(8, 10));
+    const minute = parseInt(time.substring(10, 12));
+    const second = parseInt(time.substring(12, 14));
 
-      // 스케줄러를 사용하여 예약된 시간에 알림 발송
-      schedule.scheduleJob(scheduleTime, async function () {
-        await scheduleNotification(messagePayload);
-      });
+    const scheduleTime = new Date(year, month, day, hour, minute, second);
 
-      res.status(200).json({
-        success: `성공적으로 알림을 예약했습니다.`,
-        utcTime: scheduleTime,
-        kstTime: scheduleTime.toLocaleString("ko-KR"),
-      });
-    }
-  } catch (error) {
-    console.error("에러 로그:", error);
-    res.status(500).json({ error: "알림을 발송하지 못했습니다." });
+    // 스케줄러를 사용하여 예약된 시간에 알림 발송
+    schedule.scheduleJob(scheduleTime, async function () {
+      await scheduleNotification(messagePayload);
+    });
+
+    res.status(200).json({
+      success: `성공적으로 알림을 예약했습니다.`,
+      utcTime: scheduleTime,
+      kstTime: scheduleTime.toLocaleString("ko-KR"),
+    });
+  } else {
+    // time이 제공되지 않는 경우 즉시 알림 발송
+    await scheduleNotification(messagePayload);
+
+    res.status(200).json({
+      success: `성공적으로 알림을 즉시 발송했습니다.`,
+    });
   }
 });
 
@@ -113,4 +124,3 @@ app.listen(port, () => {
 https.createServer(credentials, app).listen(8080, () => {
   console.log(`HTTPS server started on port 8080`);
 });
-
